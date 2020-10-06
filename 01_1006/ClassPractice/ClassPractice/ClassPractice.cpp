4a42d2000fd5771005b0338bd2e62d197cfcde09
MSI-沖原しずく\shizu <0uu51r1938f4b3h@au.com>
2020-10-06 13:50:10 +0900

    リンファクタリング前、ステータスを画面に表示する処理を追加

--- 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -7,7 +7,7 @@ public:
 	//コンストラクタ
 	Fighter();
 	//デストラクタ
-	Fighter();
+	~Fighter();
 
 	int m_hp;
 	int m_str;
@@ -21,16 +21,34 @@ public:
 
 Fighter::Fighter()
 {
+	m_hp  = 0;
+	m_str = 0;
+	m_mag = 0;
+	m_tec = 0;
+	m_spd = 0;
+	m_def = 0;
+	m_iuc = 0;
+	m_mde = 0;
+};
 
-}
-
-Fighter::Fighter()
+Fighter::~Fighter()
 {
 
 }
 
 int main()
 {
+	Fighter fighter();
+
+	printf("m_hpz%d\n", fighter.m_hp);
+	printf("m_strz%d\n", fighter.m_str);
+	printf("m_magz%d\n", fighter.m_mag);
+	printf("m_tecz%d\n", fighter.m_tec);
+	printf("m_spdz%d\n", fighter.m_spd);
+	printf("m_defz%d\n", fighter.m_def);
+	printf("m_iucz%d\n", fighter.m_iuc);
+	printf("m_mdez%d\n", fighter.m_mde);
+
 	system("pause");
 	return 0;
 }
